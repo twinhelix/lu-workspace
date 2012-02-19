e96dabe3bee2c0033f7211b1dc00d1f15243b1aa
sneakystarfish@gmail.com <sneakystarfish@gmail.com@d9831ec3-8f40-f81d-642c-726578ecb1ea>
2012-02-19 23:25:26 +0000

    partially done

--- trunk/CompFinanceCW1/Question1.m
+++ trunk/CompFinanceCW1/Question1.m
@@ -50,6 +50,12 @@ tab = [lambdas; pricesA; pricesB; pricesC]'
 
 plot(lambdas, pricesA, lambdas, pricesB, lambdas, pricesC);
 
+
+%legend('Bond A', 'Bond B', 'Bond C');
+%xlabel('Std Dev');
+%ylabel('Expected Return of Portfolio');
+%title('True Efficient Frontier');
+
 % ---------------------------------------------------------------- %
 end
 
